d74f5a66272847297d8d6c0d87549421161daafa
allenfwj <374356735@qq.com>
2019-03-12 17:11:29 +0800

    首頁

--- src/js/index.js
+++ src/js/index.js
@@ -1,49 +1,60 @@
+﻿
+import '../assets/font/iconfont.css'
 import '../css/public.css'
 import '../css/style.css'
 import '../css/m_style.css'
-import '../assets/font/iconfont.css'
-import '../css/bootstrap.min.css'
-import '../css/j_style.css'
+
+
+//swiper
+import Swiper from 'swiper';
 import 'swiper/dist/css/swiper.css';
-// app.use(express.static(path.join(__dirname, 'dist')));
+var swiper = new Swiper('.service.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.service.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        prevEl: '.swiper-left-button',
+        nextEl: '.swiper-right-button',
+      }
+    });
+
+var swiper = new Swiper('.company_news.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      pagination: {
+        el: '.company_news.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        prevEl: '.index-weblog-swiper-a1',
+        nextEl: '.index-weblog-swiper-a2',
+      }
+    });
+
+var swiper = new Swiper('.company_news-1.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      }
+    });
 
 
-
-//导航下拉
-$('.extra').mouseover(function(){
-    $(this).addClass("nav-hover");
-})
-$('.extra').mouseout(function(){
-    $(this).removeClass("nav-hover");
-})
-
-
-
-//给导航添加选中项
-const _menuName='data-menuName';
-const _dataValueArr = earlPublic.getAllValue(_menuName);//获取data-menuName的值
-const htmlName=earlPublic.getHtmlDocName();//获取html名称
-if(!htmlName){
-    let _thisEle=document.querySelector('['+ _menuName + '='+ _dataValueArr[0] +']');
-    _thisEle.classList.add('selected');
+var s="";
+s=document.body.clientWidth;
+if(s>=700){
+	$(".index-wedo-left").find("img").css("opacity","0");
+	$(".index-wedo-left").find("img").eq(0).css("opacity","10");
+	$(".index-wedo-left-p").eq(0).find("p").css("color","#fe6321");
+	$(".index-wedo-left-p").click(function(){
+		$(".index-wedo-left").find("img").css("opacity","0");
+		$(this).prev().css("opacity","10");
+		$(".index-wedo-left-p p").css("color","#a09d9d");
+		$(this).find("p").css("color","#fe6321");
+	})
 }
-const ele=document.querySelector('['+ _menuName +']')
-for(let i=0; i<_dataValueArr.length; i++){
-    if(htmlName==_dataValueArr[i]){
-        let _thisEle=document.querySelector('['+ _menuName +'='+ htmlName +']');
-        _thisEle.classList.add('selected');
-    }
-}
-// 'product_detail','product_list'
-function setOtherMenu(showName,setName){//详情页导航选中状态是父类
-    if(htmlName==showName){
-        let _thisEle=document.querySelector('['+ _menuName +'='+ setName +']');
-        _thisEle.classList.add('selected'); 
-    }else{
-        return false;
-    }
-}
-
-setOtherMenu('product_detail','product_list');
-setOtherMenu('news_detail','news_list');
-
